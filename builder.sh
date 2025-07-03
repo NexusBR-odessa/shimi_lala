90d689184de8a217dd26030d5dfef4aba1012727
Frost444 <thiagojurema20@gmail.com>
2025-07-03 13:36:16 -0300

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -29,7 +29,7 @@ mkdir lineage2
 cd lineage2
 
 # init repo
-repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs --depth=1
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs --depth=1
 
 # clone manifest
 git clone https://github.com/Frost444/local_manifests_odessa.git -b main15 .repo/local_manifests
