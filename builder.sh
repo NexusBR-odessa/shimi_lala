cd3e7dbc946999efc2c84ca202233f3c60903dfa
NexusBR-odessa <miguel03barreto@gmail.com>
2025-08-31 23:37:01 -0300

    LineageOS bring up

--- builder.sh
+++ builder.sh
@@ -29,7 +29,7 @@ mkdir lineage2
 cd lineage2
 
 # init repo
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b sixteen --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 
 # clone manifest
 git clone https://github.com/NexusBR-odessa/local_manifests_odessa.git -b main15 .repo/local_manifests
