a2128291e6c47e443b0a92bf77cece4d2ae871d4
NexusBR-odessa <miguel03barreto@gmail.com>
2026-03-30 19:50:34 -0300

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,3 +1,9 @@
+# Update repos
+sudo apt update
+
+# Install dependencies
+sudo apt install bc bison build-essential kmod ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+
 # setup repo
 mkdir ~/bin
 
